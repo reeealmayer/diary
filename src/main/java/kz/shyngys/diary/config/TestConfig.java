cdf34d897bacc7828592dd1a3cae464f92685c65
rilmayer <yessenbayev.chingis@gmail.com>
2025-07-23 16:09:36 +0500

    added validation and min max for property size

--- src/main/java/kz/shyngys/diary/config/TestConfig.java
+++ src/main/java/kz/shyngys/diary/config/TestConfig.java
@@ -1,14 +1,20 @@
 package kz.shyngys.diary.config;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Component
+@Validated
+@Data
 @ConfigurationProperties(prefix = "test")
 public class TestConfig {
-    @Setter
-    @Getter
     private String property;
+
+    @Max(value = 25, message = "must be between 5 and 25")
+    @Min(value = 5, message = "must be between 5 and 25")
+    private int size;
 }
