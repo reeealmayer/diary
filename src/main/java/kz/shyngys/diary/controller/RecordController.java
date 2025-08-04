db8a6099748d5c49e0b7abb764a226626fa7a00a
rilmayer <yessenbayev.chingis@gmail.com>
2025-08-04 13:16:56 +0500

    - DeleteMapping -> PatchMaping for deactivating

--- src/main/java/kz/shyngys/diary/controller/RecordController.java
+++ src/main/java/kz/shyngys/diary/controller/RecordController.java
@@ -12,8 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -79,11 +79,11 @@ public class RecordController {
         return ResponseEntity.ok(recordDto);
     }
 
-    @DeleteMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Void> deactivate(@PathVariable Long id) {
         log.info(DELETE_RECORD_API, id);
         recordService.deactivate(id);
         log.info(DELETED_RECORD_API, id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().build();
     }
 }
